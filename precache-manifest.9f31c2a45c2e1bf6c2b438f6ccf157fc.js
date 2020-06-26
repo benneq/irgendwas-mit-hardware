bda1454333db03cf6b7016aa1508f37b278f343e
benneq <benneq@users.noreply.github.com>
2020-06-26 16:04:14 +0000

    deploy: 57ccbc2708805089beed191757d5f738a5d2bbcb

---
+++ precache-manifest.9f31c2a45c2e1bf6c2b438f6ccf157fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c815547c88e5fbb5b5b5d7b45cf4459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8c0a0bfe9d0aa6d8faf",
+    "url": "/static/js/2.9e4bfad0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9e4bfad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73fe39456dbe083dd67",
+    "url": "/static/js/main.9bb69b17.chunk.js"
+  },
+  {
+    "revision": "fe14a5e8634a430a44c2",
+    "url": "/static/js/runtime-main.4a28ac0a.js"
+  }
+]);
