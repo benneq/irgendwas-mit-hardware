d7014b38d6fb050651e99aefcb23f97eb04c23ba
benneq <1627305+benneq@users.noreply.github.com>
2020-06-26 21:58:15 +0200

    Fix NumberField NaN behavior

--- src/util/NumberField.tsx
+++ src/util/NumberField.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { TextField, TextFieldProps } from '@material-ui/core';
 
-type Props = Omit<TextFieldProps, 'onChange'> & {
+type Props = Omit<TextFieldProps, 'value' | 'onChange'> & {
     value: number
     onChange?: (e: Omit<React.ChangeEvent<HTMLInputElement>, 'target'> & { target: { value: number }}) => void
 }
 
 const NumberField: React.FunctionComponent<Props> = (props) => {
+    const value = isFinite(props.value) ? props.value.toString() : '';
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         props.onChange && props.onChange({
             ...e,
@@ -20,6 +22,7 @@ const NumberField: React.FunctionComponent<Props> = (props) => {
     return (
         <TextField
             {...props}
+            value={value}
             onChange={handleChange}
         />
     );
