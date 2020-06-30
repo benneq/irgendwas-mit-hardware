ffc7fddca70c2187de516bad6a0a5028c2ab7c23
benneq <1627305+benneq@users.noreply.github.com>
2020-06-30 06:46:02 +0200

    More Wi-Fi information

--- src/network/wifi/NetworkWiFiStandards.tsx
+++ src/network/wifi/NetworkWiFiStandards.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import SimpleTable from '../../util/SimpleTable';
 
 const wifiStandards = [
-    { name: 'Wi-Fi 1', ieeeStandard: '802.11b', transferRate: '1-11 Mbit/s', frequencies: ['2,4 GHz'], notes: 'inoffizieller Name' },
-    { name: 'Wi-Fi 2', ieeeStandard: '802.11a', transferRate: '1,5-54 Mbit/s', frequencies: ['5 GHz'], notes: 'inoffizieller Name' },
-    { name: 'Wi-Fi 3', ieeeStandard: '802.11g', transferRate: '3-54 Mbit/s', frequencies: ['2,4 GHz'], notes: 'inoffizieller Name' },
-    { name: 'Wi-Fi 4', ieeeStandard: '802.11n', transferRate: '72-600 Mbit/s', frequencies: ['2,4 GHz', '5 GHz'], notes: '' },
-    { name: 'Wi-Fi 5', ieeeStandard: '802.11ac', transferRate: '433-6933 Mbit/s', frequencies: ['2,4 GHz'], notes: '' },
-    { name: 'Wi-Fi 6', ieeeStandard: '802.11ax', transferRate: '600-9608 Mbit/s', frequencies: ['2,4 GHz', '5 GHz'], notes: '' },
+    { name: 'Wi-Fi 1', ieeeStandard: '802.11b', transferRate: '1-11 Mbit/s', frequencies: ['2,4 GHz'], notes: ['inoffizielle Bezeichnung'] },
+    { name: 'Wi-Fi 2', ieeeStandard: '802.11a', transferRate: '1,5-54 Mbit/s', frequencies: ['5 GHz'], notes: ['inoffizielle Bezeichnung'] },
+    { name: 'Wi-Fi 3', ieeeStandard: '802.11g', transferRate: '3-54 Mbit/s', frequencies: ['2,4 GHz'], notes: ['inoffizielle Bezeichnung'] },
+    { name: 'Wi-Fi 4', ieeeStandard: '802.11n', transferRate: '72-600 Mbit/s', frequencies: ['2,4 GHz', '5 GHz'], notes: ['bis zu 4x4 MIMO'] },
+    { name: 'Wi-Fi 5', ieeeStandard: '802.11ac', transferRate: '433-6933 Mbit/s', frequencies: ['5 GHz'], notes: ['bis zu 8x8 MIMO', 'unterstüzt MU-MIMO ab Wave 2', 'unterstüzt Beamforming'] },
+    { name: 'Wi-Fi 6', ieeeStandard: '802.11ax', transferRate: '600-9608 Mbit/s', frequencies: ['2,4 GHz', '5 GHz'], notes: [] },
 ];
 
 const NetworkWiFiStandards: React.FunctionComponent = () => {
@@ -19,7 +19,7 @@ const NetworkWiFiStandards: React.FunctionComponent = () => {
                 { id: 'ieeeStandard', header: 'IEEE Standard', render: (row) => row.ieeeStandard },
                 { id: 'transferRate', header: 'Übertragungsrate', render: (row) => row.transferRate },
                 { id: 'frequencies', header: 'Frequenzbereich', render: (row) => row.frequencies.map((frequency, i) => <div key={i}>{frequency}</div>) },
-                { id: 'notes', header: 'Anmerkungen', render: (row) => row.notes },
+                { id: 'notes', header: 'Anmerkungen', render: (row) => row.notes.map((note, i) => <div key={i}>{note}</div>) },
             ]}
             value={wifiStandards}
         />
