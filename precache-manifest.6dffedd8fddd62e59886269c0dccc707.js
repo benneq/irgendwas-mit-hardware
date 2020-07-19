ff2a9cb323faff589cc839f120cbd39ac3bf7cb9
benneq <benneq@users.noreply.github.com>
2020-07-19 12:02:36 +0000

    deploy: 5e0c8b78ff5759954d49b058b79fa31144b8932c

--- precache-manifest.6dffedd8fddd62e59886269c0dccc707.js
+++ precache-manifest.6dffedd8fddd62e59886269c0dccc707.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbb03e840cb6708fe9c99c020fe366ba",
+    "revision": "bb23de0bf0cf9d67789eb7c5ff9aed80",
     "url": "/irgendwas-mit-hardware/index.html"
   },
   {
-    "revision": "a6e1cb207c96d6dcc588",
+    "revision": "3847d9922d343a31bc7f",
     "url": "/irgendwas-mit-hardware/static/css/main.72d5f6dc.chunk.css"
   },
   {
-    "revision": "5b4ce19351e1b2a3c151",
-    "url": "/irgendwas-mit-hardware/static/js/2.e0d8edea.chunk.js"
+    "revision": "29a44938e27d29155b4e",
+    "url": "/irgendwas-mit-hardware/static/js/2.9b75810f.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/irgendwas-mit-hardware/static/js/2.e0d8edea.chunk.js.LICENSE.txt"
+    "url": "/irgendwas-mit-hardware/static/js/2.9b75810f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a6e1cb207c96d6dcc588",
-    "url": "/irgendwas-mit-hardware/static/js/main.a99ed021.chunk.js"
+    "revision": "3847d9922d343a31bc7f",
+    "url": "/irgendwas-mit-hardware/static/js/main.d6f65ada.chunk.js"
   },
   {
     "revision": "562b5d22b332784487dd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/irgendwas-mit-hardware/precache-manifest.ee01ea9c8275034e99ca134ee9c36328.js"
+  "/irgendwas-mit-hardware/precache-manifest.6dffedd8fddd62e59886269c0dccc707.js"
 );
 
 self.addEventListener('message', (event) => {
