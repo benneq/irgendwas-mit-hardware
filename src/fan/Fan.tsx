a70c9ff4d6c8a7776576d17b5df8ff143f12d3ae
benneq <1627305+benneq@users.noreply.github.com>
2020-08-06 15:19:07 +0200

    FanSpecs

--- src/fan/Fan.tsx
+++ src/fan/Fan.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import FanConnectors from './fan-connectors.mdx';
+import FanSpecs from './fan-specs.mdx';
 
 
 const Fan: React.FunctionComponent = () => {
     return (
-        <FanConnectors />
+        <>
+            <FanConnectors />
+            <FanSpecs />
+        </>
     );
 };
 
