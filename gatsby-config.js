4a95efb451fc826f7e217e972a2beb064a69edb1
benneq <1627305+benneq@users.noreply.github.com>
2020-08-08 02:43:13 +0200

    gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,16 @@ module.exports = {
         path: `${__dirname}/src/pages`
       }
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Irgendwas mit Hardware`,
+        start_url: `/`,
+        background_color: `#fafafa`,
+        theme_color: `#3f51b5`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.png`,
+      },
+    },
   ],
 }
