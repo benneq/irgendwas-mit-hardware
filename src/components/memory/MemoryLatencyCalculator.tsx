ea0576969c91f1b654aa6e6ee352d4022e430fb1
benneq <1627305+benneq@users.noreply.github.com>
2020-08-13 03:29:56 +0200

    MemoryLatencyCalculator clean up

--- src/components/memory/MemoryLatencyCalculator.tsx
+++ src/components/memory/MemoryLatencyCalculator.tsx
@@ -1,32 +1,60 @@
 import React, { useState } from 'react';
 import { Grid } from '@material-ui/core';
 import NumberField from '../common/NumberField';
-import { MemoryLatencyPreset } from './model/memory-latency-preset.model';
-import { MemoryType } from './model/memory-type.model';
-import { memoryLatencyPresetOf, calculateFrequencyMultiplier, calculateClockCycleTime, calculateClockCyclesPerNs, calculateLatency } from './util/memory-latency.util';
 import SelectField from '../common/SelectField';
 import { isEqual } from 'lodash-es';
+import { ValueOf } from '../../utils/types.util';
+
+
+
+const sdramTypeOf = (name: string, multiplier: number) => {
+    return {
+        name,
+        multiplier
+    }
+};
+
+const memoryLatencyPresetOf = (type: ValueOf<typeof SDRAM_TYPE>, frequency: number, timing: number) => {
+    return { type, frequency, timing };
+};
+
+
+const calculateClockCycleTime = (frequency: number, frequencyMultiplier: number): number => {
+    return frequency / (frequencyMultiplier * 1000);
+};
+
+const calculateClockCyclesPerNs = (clockCycleTime: number): number => {
+    return 1 / clockCycleTime;
+};
+
+const calculateLatency = (timing: number, clockCyclesPerNs: number): number => {
+    return timing * clockCyclesPerNs;
+};
+
+const SDRAM_TYPE = {
+    'SDR': sdramTypeOf('SDR', 1),
+    'DDR': sdramTypeOf('DDR', 2),
+};
+
 
 
 const presets = [
-    memoryLatencyPresetOf('DDR', 2133, 10),
-    memoryLatencyPresetOf('DDR', 2400, 12),
-    memoryLatencyPresetOf('DDR', 3200, 16),
-    memoryLatencyPresetOf('DDR', 3600, 18),
+    memoryLatencyPresetOf(SDRAM_TYPE.DDR, 2133, 10),
+    memoryLatencyPresetOf(SDRAM_TYPE.DDR, 2400, 12),
+    memoryLatencyPresetOf(SDRAM_TYPE.DDR, 3200, 16),
+    memoryLatencyPresetOf(SDRAM_TYPE.DDR, 3600, 18),
 ];
 
 const MemoryLatencyCalculator: React.FunctionComponent = () => {
-    const [value, setValue] = useState<MemoryLatencyPreset>(presets[2]);
+    const [value, setValue] = useState(presets[2]);
     const { type, frequency, timing } = value;
 
-    const frequencyMultiplier = calculateFrequencyMultiplier(type);
-    const clockCycleTime = calculateClockCycleTime(frequency, frequencyMultiplier);
+    const clockCycleTime = calculateClockCycleTime(frequency, type.multiplier);
     const clockCyclesPerNs = calculateClockCyclesPerNs(clockCycleTime);
     const latency = calculateLatency(timing, clockCyclesPerNs);
-    const bandwidth = 64*frequency/frequencyMultiplier/8/1000;
 
-    const handleTypeChange = (e: React.ChangeEvent<{ value: unknown }>) => {
-        setValue(value => ({ ...value, type: e.target.value as MemoryType }));
+    const handleTypeChange = (val: ValueOf<typeof SDRAM_TYPE>) => {
+        setValue(value => ({ ...value, type: val }));
     };
 
     const handleFrequencyChange = (val?: number) => {
@@ -39,80 +67,74 @@ const MemoryLatencyCalculator: React.FunctionComponent = () => {
 
     return (
         <>
-            <Grid container>
+            <Grid container direction="column" spacing={2}>
                 <Grid item>
                     <SelectField
                         label="Presets"
                         value={value}
                         options={presets}
-                        renderOption={preset => `${preset.type} ${preset.frequency}MHz CL${preset.timing}`}
+                        renderOption={preset => `${preset.type.name}-${preset.frequency} CL${preset.timing}`}
                         onValueChange={setValue}
                         valueComparator={isEqual}
                     >
                         Custom
                     </SelectField>
                 </Grid>
-            </Grid>
-            <Grid container spacing={2}>
                 <Grid item>
-                    <SelectField<MemoryType>
-                        label="Typ"
-                        value={type}
-                        options={["SDR", "DDR"]}
-                        onChange={handleTypeChange}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Speichertakt"
-                        helperText="In MHz"
-                        value={frequency}
-                        onValueChange={handleFrequencyChange}
-                        allowNegative={false}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Taktzyklusdauer"
-                        helperText="In ns"
-                        disabled
-                        value={clockCycleTime}
-                        allowNegative={false}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Taktzyklen pro ns"
-                        helperText="Taktzyklen"
-                        disabled
-                        value={clockCyclesPerNs}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Timing"
-                        helperText="Taktzyklen"
-                        value={timing}
-                        onValueChange={handleTimingChange}
-                        allowNegative={false}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Effektive Latenz"
-                        helperText="In ns"
-                        disabled
-                        value={latency}
-                    />
-                </Grid>
-                <Grid item>
-                    <NumberField
-                        label="Bandbreite (pro Modul)"
-                        helperText="In GB/sec"
-                        disabled
-                        value={bandwidth}
-                        allowNegative={false}
-                    />
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <SelectField
+                                label="Typ"
+                                value={value.type}
+                                options={[SDRAM_TYPE.SDR, SDRAM_TYPE.DDR]}
+                                renderOption={type => type.name}
+                                onValueChange={handleTypeChange}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <NumberField
+                                label="Effektive Taktfrequenz"
+                                helperText="In MHz"
+                                value={frequency}
+                                onValueChange={handleFrequencyChange}
+                                allowNegative={false}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <NumberField
+                                label="Taktzyklusdauer"
+                                helperText="In ns"
+                                disabled
+                                value={clockCycleTime}
+                                allowNegative={false}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <NumberField
+                                label="Taktzyklen pro ns"
+                                helperText="Taktzyklen"
+                                disabled
+                                value={clockCyclesPerNs}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <NumberField
+                                label="Timing"
+                                helperText="Taktzyklen"
+                                value={timing}
+                                onValueChange={handleTimingChange}
+                                allowNegative={false}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <NumberField
+                                label="Effektive Latenz"
+                                helperText="In ns"
+                                disabled
+                                value={latency}
+                            />
+                        </Grid>
+                    </Grid>
                 </Grid>
             </Grid>
         </>
